9650e1d1799e3ddd37f3d0c41d32bcab2f32afc9
MOU301 <mooramadan@gmail.com>
2024-07-24 00:44:28 +0200

    test3

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 include 'core/init.php';
 $template=new template('templates/HomePage.php');
 $template->title="mohammad ";
-
-
+$template->name='ramadan';
+$template->jobtotal=["web","front_end","back end","seo"];
 echo $template;
 ?>
--- templates/HomePage.php
+++ templates/HomePage.php
@@ -1,1 +1,4 @@
 <h1><?php echo $title; ?></h1>
+<?php foreach($jobtotal as $n=>$job): ?>
+    <h5><span><?php echo $n."------" ;?></span><?php echo $job; ?></h5>
+<?php endforeach ;?>
